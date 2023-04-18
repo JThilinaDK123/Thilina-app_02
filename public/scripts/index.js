fd282555bd80469fa39642f39fcda89c2eb7ef36
JThilinaDK123 <JThilinaDK123@gmail.com>
2023-04-18 17:20:56 +0530

    forth comk

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,34 +1,36 @@
-/**
- * These lines of code are selecting HTML elements from the DOM (Document Object Model) using their respective IDs and assigning them to variables using the `const` keyword. 
- * */
-const sendMsg = document.getElementById('msg-send');
-const inputMsg = document.getElementById('msg-input');
-const resMsg = document.getElementById('msg-response');
+require('dotenv').config();
 
+const express = require('express');
+const app = express();
 
-/**
- * This function sends a message inputted by the user to the server and displays the response received from the server.
- * @param e - The parameter `e` is an event object that is passed to the `sendMsgInfo` function when it is called. It is used to prevent the default behavior of a form submission when the function is called as an event listener.
- * @returns If the inputMsg value is empty, the function will return and nothing will happen. Otherwise, the function will make a POST request to the '/inputMsg' endpoint with the inputMsg value as a JSON object in the request body. It will then wait for a response from the server and if the response status is 'received', it will set the text content of the resMsg element to the response
- */
-const sendMsgInfo = async (e) => {
-  e.preventDefault();
-  if (inputMsg.value === '') { return };
-  const res = await fetch('/inputMsg', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ parcel: inputMsg.value }),
-  });
-  const data = await res.json();
-  if (data.status === 'recieved') {
-    resMsg.textContent = data.message;
-  }
+const mongoose = require('mongoose');
+// console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const Schema = mongoose.Schema;
+const responseSchema = new Schema({
+    response: { type: String, required: true }
+});
+const Response = mongoose.model("Response", responseSchema);
+
+const createAndSaveResponse = (apiResponse, done) => {
+    apiResponse.save(function(err, data) {
+        if (err) return console.error(err);
+        done(null, data);
+    });
 };
 
-/**
- * `sendMsg.addEventListener('click', sendMsgInfo);` is adding an event listener to the `sendMsg` element. The event listener is listening for a 'click' event on the `sendMsg` element, and when the event is triggered, it calls the `sendMsgInfo` function. 
- * This allows the user to click the `sendMsg` element and trigger the `sendMsgInfo` function, which sends a message inputted by the user to the server and displays the response received from the server. 
- * */
-sendMsg.addEventListener('click', sendMsgInfo);
+// localhost:3000
+app.get('/', function (req, res) {
+    res.send('{ "response": "Hello From Thilina9718" }');
+});
+
+app.get('/will', function (req, res) {
+    res.send('{ "response": "Will endpoint works" }');
+});
+
+app.get('/ready', function (req, res) {
+    res.send('{ "response": " Great!, It works!" }');
+});
+
+app.listen(process.env.PORT || 8089);
+module.exports = app;
